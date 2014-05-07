76aaee3c9db1a08053a57ba4f9d8fcd5d14cf568
Changsheng Jiang <jiangzuoyan@gmail.com>
2014-05-07 15:30:02 +0200

    Refine CLOG
    Add EveryProgress
    
    Turn CLOG_CHECK off when passing.

--- clog/clog.hpp
+++ clog/clog.hpp
@@ -6,6 +6,7 @@
 #include <streambuf>
 #include <mutex>
 #include <iostream>
+#include <chrono>
 
 namespace clog {
 
@@ -82,6 +83,7 @@ struct message_buffer {
   }
 };
 
+#define CLOG_SEVERITY_LEVEL_NONE 0
 #define CLOG_SEVERITY_LEVEL_TRACE 1000
 #define CLOG_SEVERITY_LEVEL_DEBUG 2000
 #define CLOG_SEVERITY_LEVEL_INFO  3000
@@ -89,10 +91,11 @@ struct message_buffer {
 #define CLOG_SEVERITY_LEVEL_ERROR 10000
 #define CLOG_SEVERITY_LEVEL_FATAL 100000
 
+#define CLOG_SEVERITY_NAME_NONE 'N'
 #define CLOG_SEVERITY_NAME_TRACE 'T'
 #define CLOG_SEVERITY_NAME_DEBUG 'D'
-#define CLOG_SEVERITY_NAME_INFO  'I'
-#define CLOG_SEVERITY_NAME_WARN  'W'
+#define CLOG_SEVERITY_NAME_INFO 'I'
+#define CLOG_SEVERITY_NAME_WARN 'W'
 #define CLOG_SEVERITY_NAME_ERROR 'E'
 #define CLOG_SEVERITY_NAME_FATAL 'F'
 
@@ -104,15 +107,27 @@ struct message_buffer {
 #define CLOG_SEVERITY_NAME_DFATAL CLOG_SEVERITY_NAME_ERROR
 #endif
 
-bool log_severity(int level) {
+namespace {
 #ifndef NDEBUG
-  return true;
+int log_level = 1;
+int log_verbose = 0;
 #else
-  return level >= CLOG_SEVERITY_LEVEL_INFO;
+int log_level = CLOG_SEVERITY_LEVEL_INFO;
+int log_verbose = 0;
 #endif
+}  // namespace
+
+template <class T>
+void avoid_never_used() {
+  log_level = 0;
+  log_verbose = 0;
 }
 
-void message_skip(message_buffer &mb) { mb.keep = false; }
+inline bool log_severity(int level) {
+  return level >= log_level;
+}
+
+inline void message_skip(message_buffer &mb) { mb.keep = false; }
 
 inline message_buffer &operator<<(message_buffer &mb,
                                   void (*func)(message_buffer &)) {
@@ -124,7 +139,7 @@ inline void message(message_buffer &mb) {
   static std::mutex mutex;
   std::lock_guard<std::mutex> lk(mutex);
   static bool last_keep = true;
-  if (!last_keep && !mb.keep) {
+  if (!last_keep) {
     std::cerr << "\x1b[" << 1 << "A\x1b[K";
   }
   last_keep = mb.keep;
@@ -148,56 +163,294 @@ inline void message(message_buffer &mb) {
   if (mb.level >= CLOG_SEVERITY_LEVEL_FATAL) abort();
 }
 
-#define CLOG_PP_VAR(NAME) PP_CAT(CLOG_PP_VAR_, PP_CAT(NAME, __LINE__))
+#define CLOG_PP_VAR(NAME, COUNTER) PP_CAT(CLOG_PP_VAR_, PP_CAT(NAME, COUNTER))
 
-#define CLOG_OUTPUT(ignored, X)           \
-  {                                       \
-    if (CLOG_PP_VAR(buffer).num_fields) { \
-      CLOG_PP_VAR(buffer) << ' ';         \
-    }                                     \
-    CLOG_PP_VAR(buffer) << X;             \
+#define CLOG_OUTPUT(COUNTER, X)                    \
+  {                                                \
+    if (CLOG_PP_VAR(buffer, COUNTER).num_fields) { \
+      CLOG_PP_VAR(buffer, COUNTER) << ' ';         \
+    }                                              \
+    CLOG_PP_VAR(buffer, COUNTER) << X;             \
   }
 
-#define CLOG_GUARD(GUARD, SEVERITY_NUM, SEVERITY_NAME, ...)                 \
-  do {                                                                      \
-    if ((GUARD) && clog::log_severity(SEVERITY_NUM)) {                      \
-      clog::message_buffer CLOG_PP_VAR(buffer)(SEVERITY_NUM, SEVERITY_NAME, \
-                                               __FILE__, __LINE__);         \
-      PP_FOREACH(CLOG_OUTPUT, ~, __VA_ARGS__);                              \
-      clog::message(CLOG_PP_VAR(buffer));                                   \
-    }                                                                       \
+#define CLOG_(COUNTER, SEVERITY_NUM, SEVERITY_NAME, ...)    \
+  do {                                                      \
+    if (::clog::log_severity(SEVERITY_NUM)) {               \
+      ::clog::message_buffer CLOG_PP_VAR(buffer, COUNTER)(  \
+          SEVERITY_NUM, SEVERITY_NAME, __FILE__, __LINE__); \
+      PP_FOREACH(CLOG_OUTPUT, COUNTER, ##__VA_ARGS__);      \
+      clog::message(CLOG_PP_VAR(buffer, COUNTER));          \
+    }                                                       \
   } while (0)
 
+#define CLOG(SEVERITY, ...)                          \
+  CLOG_(__COUNTER__, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+        CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+
 #ifndef NDEBUG
-#define CLOG_DEBUG_GUARD true
+#define DCLOG CLOG
 #else
-#define CLOG_DEBUG_GUARD false
+#define DCLOG(...)
 #endif
 
-#define CLOG(SEVERITY, ...)                        \
-  CLOG_GUARD(true, CLOG_SEVERITY_LEVEL_##SEVERITY, \
-             CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+struct EveryN {
+  int counter = 0;
 
-#define DCLOG(SEVERITY, ...)                                   \
-  CLOG_GUARD(CLOG_DEBUG_GUARD, CLOG_SEVERITY_LEVEL_##SEVERITY, \
-             CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+  bool should_log(int n) { return n != 0 && (counter++ % n) == 0; }
+};
 
-#define CLOG_CHECK(EXPR, ...)                                                  \
+#define CLOG_EVERY_N_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
+  do {                                                              \
+    static ::clog::EveryN CLOG_PP_VAR(every_n, COUNTER);            \
+    if (CLOG_PP_VAR(every_n, COUNTER).should_log(N)) {              \
+      CLOG_(COUNTER, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__);   \
+    }                                                               \
+  } while (0)
+
+#define CLOG_EVERY_N(N, SEVERITY, ...)                          \
+  CLOG_EVERY_N_(__COUNTER__, N, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+
+#ifndef NDEBUG
+#define DCLOG_EVERY_N CLOG_EVERY_N
+#else
+#define DCLOG_EVERY_N(...)
+#endif
+
+struct FirstN {
+  int counter = 0;
+
+  bool should_log(int n) { return counter++ < n; }
+};
+
+#define CLOG_FIRST_N_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
+  do {                                                              \
+    static ::clog::FirstN CLOG_PP_VAR(first_n, COUNTER);            \
+    if (CLOG_PP_VAR(first_n, COUNTER).should_log(N)) {              \
+      CLOG_(COUNTER, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__);   \
+    }                                                               \
+  } while (0)
+
+#define CLOG_FIRST_N(N, SEVERITY, ...)                       \
+  CLOG_FIRST_N_(__COUNTER__, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+
+struct EverySec {
+  std::chrono::time_point<std::chrono::steady_clock> next_log_time;
+
+  bool should_log(double n) {
+    auto now = std::chrono::steady_clock::now();
+    if (now >= next_log_time) {
+      next_log_time =
+          now + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                    std::chrono::duration<double>(n));
+      return true;
+    }
+    return false;
+  }
+};
+
+#define CLOG_EVERY_SEC_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
+  do {                                                                \
+    static ::clog::EverySec CLOG_PP_VAR(every_sec, COUNTER);          \
+    if (CLOG_PP_VAR(every_sec, COUNTER).should_log(N)) {              \
+      CLOG_(COUNTER, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__);     \
+    }                                                                 \
+  } while (0)
+
+#define CLOG_EVERY_SEC(N, SEVERITY, ...)                          \
+  CLOG_EVERY_SEC_(__COUNTER__, N, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                  CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+
+#ifndef NDEBUG
+#define DCLOG_EVERY_SEC CLOG_EVERY_SEC
+#else
+#define DCLOG_EVERY_SEC(...)
+#endif
+
+struct EveryProgress {
+  std::chrono::time_point<std::chrono::steady_clock> next_log_time;
+  size_t next_log_index = 0;
+  size_t counter = 0;
+
+  bool should_log(double percent, double sec) {
+    auto now = std::chrono::steady_clock::now();
+    if (counter++ >= next_log_index || next_log_time <= now) {
+      size_t ni = next_log_index * (1.0 + percent);
+      if (ni == next_log_index) ni ++;
+      next_log_index = ni;
+      next_log_time =
+          now + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                    std::chrono::duration<double>(sec));
+      return true;
+    }
+    return false;
+  }
+};
+
+#define CLOG_EVERY_PROGRESS_(COUNTER, PERCENT, SEC, SEVERITY_NUM,            \
+                             SEVERITY_NAME, ...)                             \
+  do {                                                                       \
+    static ::clog::EveryProgress CLOG_PP_VAR(every_progress, COUNTER);       \
+    if (CLOG_PP_VAR(every_progress, COUNTER).should_log((PERCENT), (SEC))) { \
+      CLOG_(COUNTER, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__);            \
+    }                                                                        \
+  } while (0)
+
+#define CLOG_EVERY_PROGRESS(PERCENT, SEC, SEVERITY, ...) \
+  CLOG_EVERY_PROGRESS_(__COUNTER__, PERCENT, SEC,        \
+                       CLOG_SEVERITY_LEVEL_##SEVERITY,   \
+                       CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__)
+
+#define CLOG_IF_(COUNTER, COND, SEVERITY_NUM, SEVERITY_NAME, ...)     \
+  do {                                                                \
+    if (COND) {                                                       \
+      CLOG_(__COUNTER__, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__); \
+    }                                                                 \
+  } while (0)
+
+#define CLOG_IF(COND, SEVERITY, ...)                          \
+  CLOG_IF_(__COUNTER__, COND, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+           CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DCLOG_IF CLOG_IF
+#else
+#define DCLOG_IF(...)
+#endif
+
+#define CLOG_IF_EVERY_N_(COUNTER, COND, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
+  do {                                                                       \
+    if (COND) {                                                              \
+      CLOG_EVERY_N_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__); \
+    }                                                                        \
+  } while (0)
+
+#define CLOG_IF_EVERY_N(COND, N, SEVERITY, ...)                          \
+  CLOG_IF_EVERY_N_(__COUNTER__, COND, N, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                   CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DCLOG_IF_EVERY_N CLOG_IF_EVERY_N
+#else
+#define DCLOG_IF_EVERY_N(...)
+#endif
+
+#define CLOG_IF_EVERY_SEC_(COUNTER, COND, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
   do {                                                                         \
-    if (!(EXPR)) {                                                             \
-      CLOG(FATAL, "Check " << clog::Color::red() << "failed"                   \
-                           << clog::Color::reset() << ":",                     \
-           #EXPR, ##__VA_ARGS__);                                              \
-    } else {                                                                   \
-      DCLOG(DEBUG, clog::message_skip, "Check "                                \
-                                           << clog::Color::green() << "passed" \
-                                           << clog::Color::reset() << ":",     \
-            #EXPR, ##__VA_ARGS__);                                             \
+    if (COND) {                                                                \
+      CLOG_EVERY_SEC_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__); \
     }                                                                          \
   } while (0)
 
+#define CLOG_IF_EVERY_SEC(COND, N, SEVERITY, ...)                          \
+  CLOG_IF_EVERY_SEC_(__COUNTER__, COND, N, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                     CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DCLOG_IF_EVERY_SEC CLOG_IF_EVERY_SEC
+#else
+#define DCLOG_IF_EVERY_SEC(...)
+#endif
+
+#define CLOG_IF_FIRST_N_(COUNTER, COND, N, SEVERITY_NUM, SEVERITY_NAME, ...) \
+  do {                                                                       \
+    if (COND) {                                                              \
+      CLOG_FIRST_N_(COUNTER, N, SEVERITY_NUM, SEVERITY_NAME, ##__VA_ARGS__); \
+    }                                                                        \
+  } while (0)
+
+#define CLOG_IF_FIRST_N(COND, N, SEVERITY, ...)                          \
+  CLOG_IF_FIRST_N_(__COUNTER__, COND, N, CLOG_SEVERITY_LEVEL_##SEVERITY, \
+                   CLOG_SEVERITY_NAME_##SEVERITY, ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DCLOG_IF_FIRST_N CLOG_IF_FIRST_N
+#else
+#define DCLOG_IF_FIRST_N(...)
+#endif
+
+#define VCLOG(V, ...) CLOG_IF(::clog::log_verbose >= (V), INFO, ##__VA_ARGS__);
+#define VCLOG_IF(V, COND, ...) \
+  CLOG_IF(::clog::log_verbose >= (V) && (COND), INFO, ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DVCLOG VCLOG
+#else
+#define DVCLOG(...)
+#endif
+
+#define VCLOG_EVERY_N(V, N, ...) \
+  CLOG_IF_EVERY_N(::clog::log_verbose >= (V), N, INFO, ##__VA_ARGS__);
+
+#define VCLOG_FIRST_N(V, N, ...) \
+  CLOG_IF_FIRST_N(::clog::log_verbose >= (V), N, INFO, ##__VA_ARGS__);
+
+#define VCLOG_EVERY_SEC(V, N, ...) \
+  CLOG_IF_EVERY_SEC(::clog::log_verbose >= (V), N, INFO, ##__VA_ARGS__);
+
+#define VCLOG_IF_EVERY_N(V, COND, N, ...) \
+  CLOG_IF_EVERY_N(::clog::log_verbose >= (V) && (COND), N, INFO, ##__VA_ARGS__);
+
+#define VCLOG_IF_FIRST_N(V, COND, N, ...) \
+  CLOG_IF_FIRST_N(::clog::log_verbose >= (V) && (COND), N, INFO, ##__VA_ARGS__);
+
+#define VCLOG_IF_EVERY_SEC(V, COND, N, ...)                        \
+  CLOG_IF_EVERY_SEC(::clog::log_verbose >= (V) && (COND), N, INFO, \
+                    ##__VA_ARGS__);
+
+#ifndef NDEBUG
+#define DVCLOG_EVERY_N VCLOG_EVERY_N
+#define DVCLOG_FIRST_N VCLOG_FIRST_N
+#define DVCLOG_EVERY_SEC VCLOG_EVERY_SEC
+#define DVCLOG_IF VCLOG_IF
+#define DVCLOG_IF_EVERY_N VCLOG_IF_EVERY_N
+#define DVCLOG_IF_FIRST_N VCLOG_IF_FIRST_N
+#define DVCLOG_IF_EVERY_SEC VCLOG_IF_EVERY_SEC
+#else
+#define DVCLOG_EVERY_N(...)
+#define DVCLOG_FIRST_N(...)
+#define DVCLOG_EVERY_SEC(...)
+#define DVCLOG_IF(...)
+#define DVCLOG_IF_EVERY_N(...)
+#define DVCLOG_IF_FIRST_N(...)
+#define DVCLOG_IF_EVERY_SEC(...)
+#endif
+
+#define CLOG_GEN_CHECK(EXPR, PASS_LEVEL, FAIL_LEVEL, ...)                      \
+  do {                                                                         \
+    if (!(EXPR)) {                                                             \
+      CLOG_(__COUNTER__, CLOG_SEVERITY_LEVEL_##FAIL_LEVEL,                     \
+            CLOG_SEVERITY_NAME_##FAIL_LEVEL,                                   \
+            "Check " << clog::Color::red() << "failed" << clog::Color::reset() \
+                     << ":",                                                   \
+            #EXPR, ##__VA_ARGS__);                                             \
+    } else {                                                                   \
+      CLOG_(__COUNTER__, CLOG_SEVERITY_LEVEL_##PASS_LEVEL,                     \
+            CLOG_SEVERITY_NAME_##PASS_LEVEL, clog::message_skip,               \
+            "Check " << clog::Color::green() << "passed"                       \
+                     << clog::Color::reset() << ":",                           \
+            #EXPR);                                                            \
+    }                                                                          \
+  } while (0)
+
+#ifndef NDEBUG
+#define CLOG_CHECK(EXPR, ...) CLOG_GEN_CHECK(EXPR, NONE, FATAL, ##__VA_ARGS__)
+#else
+#define CLOG_CHECK(EXPR, ...) CLOG_GEN_CHECK(EXPR, NONE, FATAL, ##__VA_ARGS__)
+#endif
+
+#ifndef NDEBUG
+#define CLOG_DCHECK(EXPR, ...) CLOG_GEN_CHECK(EXPR, NONE, FATAL, ##__VA_ARGS__)
+#else
+#define CLOG_DCHECK(EXPR, ...) CLOG_GEN_CHECK(EXPR, NONE, ERROR, ##__VA_ARGS__)
+#endif
+
 #define CLOG_CHECK_BIN(L, BIN, R, ...) \
   CLOG_CHECK(L BIN R, "(" << L, #BIN, R << ")", ##__VA_ARGS__)
+
+#define CLOG_DCHECK_BIN(L, BIN, R, ...) \
+  CLOG_DCHECK(L BIN R, "(" << L, #BIN, R << ")", ##__VA_ARGS__)
 
 #define CLOG_CHECK_EQ(L, R, ...) CLOG_CHECK_BIN(L, ==, R, ##__VA_ARGS__)
 #define CLOG_CHECK_NE(L, R, ...) CLOG_CHECK_BIN(L, !=, R, ##__VA_ARGS__)
--- clog_test.cpp
+++ clog_test.cpp
@@ -1,11 +1,61 @@
+#define NONE NONE_redefined
+#define INFO INFO_redefined
+#define ERROR ERROR_redefined
+#define WARNING WARNING_redefined
+#define FATAL FATAL_redefined
+
 #include "clog/clog.hpp"
+#include <unistd.h>
+
+#define MACRO_WITH_CLOG(A, B, C) \
+  CLOG(INFO, "here", A);         \
+  CLOG(INFO, "here", B);         \
+  CLOG(INFO, "here", C);
+
+void test_macro() { MACRO_WITH_CLOG(1, 2, 3); }
+
+void test_every_n() {
+  for (int i = 0; i < 100; ++i) {
+    CLOG_EVERY_N(10, INFO, "every 10, i=" << i);
+    CLOG_IF_EVERY_N(i & 1, 10, INFO, "every 10, i=" << i);
+  }
+}
+
+void test_every_sec() {
+  for (int i = 0; i < 100; ++i) {
+    CLOG_EVERY_SEC(0.1, INFO, "every 0.1 sec i=" << i);
+    usleep(1.e6 / 100);
+  }
+}
+
+void test_verbose() {
+  VCLOG_IF(10, false, "false");
+  VCLOG_IF(10, true, "true");
+  VCLOG(10, "verbose 10");
+  VCLOG(100, "verbose 100");
+  VCLOG(1000, "verbose 1000");
+  for (int i = 0; i < 200; ++i) {
+    VCLOG_EVERY_N(i, 10, "verbose " << i, "every 10");
+    VCLOG_IF_EVERY_N(i, (i & 1), 10, "verbose " << i, "every 10 if odd");
+    CLOG_IF_EVERY_N(::clog::log_verbose >= i, 10, INFO, "clog_if every 10 i=" << i);
+  }
+}
 
 int main(int argc, char *argv[]) {
+  clog::log_verbose = 100;
   CLOG(INFO, "start");
+  test_macro();
+  test_every_n();
+  test_every_sec();
+  test_verbose();
   CLOG_CHECK_EQ(1, 1, "1 == 1");
   for (int i = 0; i < 10000; ++i) {
     CLOG_CHECK_EQ(i, i, i, "==", i);
   }
   CLOG_CHECK_LT(1, 2, "1 < 2");
   CLOG_CHECK_EQ(1, 2, "1 == 2, fatal");
+  {
+    int i = 0;
+    CLOG_CHECK_EQ(i, 0);
+  }
 }
